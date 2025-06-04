3dca9a81cfdcf743b59d9748707c36e235c9b775
CH HASEEB <mhmodsyt517@gmail.com>
2025-06-05 00:08:15 +0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+javascript
+function goTo(page) {
+  window.location.href = page;
+}
+```
+
+---
+
+*4. PHP Files (example for one)*
+
+*`first.php`*
+```php
+<?php
+echo "<h1>This is First Page</h1>";
+?>
